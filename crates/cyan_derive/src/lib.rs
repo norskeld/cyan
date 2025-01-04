20379df4089df8ef31eeb1fa3f55f714e482daa7
Vladislav Mamon <norskeld@gmail.com>
2025-01-05 02:01:53 +0300

    Fix `Located` derive macro implementation and its error messages

--- crates/cyan_derive/src/lib.rs
+++ crates/cyan_derive/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
-use proc_macro_error::{abort, emit_error, proc_macro_error};
+use proc_macro_error::{emit_error, proc_macro_error};
 use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, Ident, Type};
+use syn::{parse_macro_input, Data, DataEnum, DataStruct, DeriveInput, Fields, Ident, Type};
 
 #[proc_macro_error]
 #[proc_macro_derive(Located)]
@@ -14,42 +14,42 @@ pub fn located_derive(input: TokenStream) -> TokenStream {
   match data {
     | Data::Struct(data_struct) => handle_struct(name, data_struct),
     | Data::Enum(data_enum) => handle_enum(name, data_enum),
-    | Data::Union(..) => abort!(name, "Located cannot be derived for unions"),
+    | Data::Union(..) => {
+      emit_error!(name, "Located cannot be derived for unions");
+
+      TokenStream::new()
+    },
   }
 }
 
 fn handle_struct(name: Ident, data_struct: DataStruct) -> TokenStream {
-  // Look for a field named "location" and of type `cyan_reporting::Location`.
-  if let Some(location_field) = data_struct.fields.iter().find(|field| {
+  let has_location_field = data_struct.fields.iter().any(|field| {
     field
       .ident
       .as_ref()
       .map_or(false, |ident| ident == "location")
-  }) {
-    let location_ident = location_field.ident.as_ref().unwrap();
+  });
 
-    return TokenStream::from(quote! {
+  if has_location_field {
+    TokenStream::from(quote! {
       impl cyan_reporting::Located for #name {
         fn location(&self) -> &cyan_reporting::Location {
-          &self.#location_ident
+          &self.location
         }
       }
-    });
+    })
+  } else {
+    emit_error!(name, "struct does not have a `location` field");
+
+    TokenStream::new()
   }
-
-  emit_error!(name, "Struct does not have a `location` field.");
-
-  TokenStream::new()
 }
 
-fn handle_enum(name: Ident, data_enum: syn::DataEnum) -> TokenStream {
+fn handle_enum(name: Ident, data_enum: DataEnum) -> TokenStream {
   let mut match_arms = Vec::new();
-  let mut expected_arms_count = 0;
 
   for variant in data_enum.variants.iter() {
     let variant_name = &variant.ident;
-
-    expected_arms_count += 1;
 
     match &variant.fields {
       // Named struct variants, e.g.
@@ -57,67 +57,63 @@ fn handle_enum(name: Ident, data_enum: syn::DataEnum) -> TokenStream {
       // enum Foo {
       //   Bar { value: i32, location: Location },
       // }
-      | Fields::Named(fields_named) => {
-        let location_field = fields_named.named.iter().find(|field| {
+      | Fields::Named(fields) => {
+        let has_location_field = fields.named.iter().any(|field| {
           field
             .ident
             .as_ref()
             .map_or(false, |ident| ident == "location")
         });
 
-        // If we found a location field, add a match arm for it.
-        if location_field.is_some() {
+        if has_location_field {
           match_arms.push(quote! {
             #name::#variant_name { location, .. } => location,
           });
-        }
-        // Otherwise, issue a warning and short-circuit with error.
-        else {
+        } else {
           emit_error!(
             variant_name,
-            "Struct variant '{}' does not have a 'location' field.",
-            variant_name
+            "struct variant does not have a `location` field"
           );
-
-          break;
         }
       },
-      // Unnamed (or tuple) variants, e.g.
+      // Unnamed (tuple) variants, e.g.
       //
       // enum Foo {
-      //   Bar(Baz, Cux),
+      //   Bar(Baz),
       // }
       //
       // This case is hard limited to only one unnamed field, which is expected to be something
-      // that implements `Located`.
-      | Fields::Unnamed(fields_unnamed) => {
-        if let Some(first_field) = fields_unnamed.unnamed.first() {
-          if let Type::Path(type_path) = &first_field.ty {
-            if type_path.path.segments.len() > 1 {
-              emit_error!(
-                variant_name,
-                "Tuple variant's field has more than one segment, which is not supported."
-              );
+      // that implements `Located`. In theory, we could merge locations of the first and the last
+      // fields. In practice, we don't need that right now.
+      | Fields::Unnamed(fields) => {
+        // If we have something like:
+        //
+        // enum Foo {
+        //   Bar(Baz, Cux),
+        // }
+        if fields.unnamed.len() > 1 {
+          emit_error!(
+            variant_name,
+            "tuple variant has more than one field, which is not supported"
+          );
 
-              continue;
-            }
+          continue;
+        }
 
-            match type_path.path.segments.first() {
-              | Some(..) => {
-                match_arms.push(quote! {
-                  #name::#variant_name(located) => located.location(),
-                });
-              },
-              | None => {
-                emit_error!(
-                  variant_name,
-                  "Tuple variant's field does not have any segments."
-                )
-              },
-            }
+        if let Some(field) = fields.unnamed.first() {
+          // Avoid producing completely broken impl.
+          if matches!(&field.ty, Type::Path(..) | Type::Reference(..)) {
+            match_arms.push(quote! {
+              #name::#variant_name(located) => located.location(),
+            });
+          } else {
+            emit_error!(variant_name, "tuple variant's field is not a valid type");
           }
         } else {
-          emit_error!(variant_name, "Tuple variant has no fields.");
+          emit_error!(
+            variant_name,
+            "tuple variant has no fields; add a single field with type deriving `Located`"
+          );
         }
       },
       // Unit variants, e.g.
@@ -128,38 +124,33 @@ fn handle_enum(name: Ident, data_enum: syn::DataEnum) -> TokenStream {
       //
       // These can't have a location field, so we error out.
       | Fields::Unit => {
-        if !match_arms.is_empty() {
-          emit_error!(variant_name, "Unit variant has no fields.");
-        }
+        emit_error!(
+          variant_name,
+          "unit variant has no fields; change to tuple or struct variant"
+        );
       },
     }
   }
 
+  // If we didn't produce any match arms, emit error and return empty token stream.
   if match_arms.is_empty() {
-    emit_error!(name, "Enum has no valid variants.");
+    emit_error!(name, "enum has no valid variants to derive for");
 
     return TokenStream::new();
   }
 
-  if !match_arms.is_empty() && expected_arms_count != match_arms.len() {
-    emit_error!(
-      name,
-      "Expected all variants to have either a `location` \
-      field or contain value that has it, but only {} of them do(es).",
-      match_arms.len()
-    );
-
-    return TokenStream::new();
-  }
-
-  // Emit the implementation with the match arms we collected.
-  TokenStream::from(quote! {
-    impl cyan_reporting::Located for #name {
-      fn location(&self) -> &cyan_reporting::Location {
-        match self {
-          #(#match_arms)*
+  // If we correctly produced all match arms, emit the implementation.
+  if data_enum.variants.len() == match_arms.len() {
+    return TokenStream::from(quote! {
+      impl cyan_reporting::Located for #name {
+        fn location(&self) -> &cyan_reporting::Location {
+          match self {
+            #(#match_arms)*
+          }
         }
       }
-    }
-  })
+    });
+  }
+
+  TokenStream::new()
 }
